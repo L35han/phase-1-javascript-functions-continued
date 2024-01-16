65deee236126e7e9a08631b862a9e818415f2f3e
Ted <ted.gitonga@student.moringaschool.com>
2024-01-16 23:26:47 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+        return `This Saturday, I want to ${activity}!`;
+     }
+function mondayWork(activity = 'go to the office') {
+        return `This Monday, I will ${activity}.`;
+    }
+// wrapAdjective function definition
+function wrapAdjective(string) {
+    return function(adjective) {
+      return `You are ${string}${adjective}${string}!`;
+    };
+  }
+
+
+  const wrapAdjectiveWithAsterisk = wrapAdjective("*");
+  console.log(wrapAdjectiveWithAsterisk("cool"));
+
